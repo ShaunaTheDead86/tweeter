241a9d2775654beb57755e0fbefbbcdaf8d832f9
Shauna Empringham <shaunathedead86@gmail.com>
2022-04-02 16:24:44 -0400

    updating packages and listen port

--- server/index.js
+++ server/index.js
@@ -1,26 +1,26 @@
-"use strict";
+'use strict';
 
 // Basic express setup:
 
 const PORT = 8080;
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-const db = require("./lib/in-memory-db");
-const DataHelpers = require("./lib/data-helpers.js")(db);
+const db = require('./lib/in-memory-db');
+const DataHelpers = require('./lib/data-helpers.js')(db);
 
 // Update the dates for the initial tweets (data-files/initial-tweets.json).
-require("./lib/date-adjust")();
+require('./lib/date-adjust')();
 
-const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+const tweetsRoutes = require('./routes/tweets')(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
-app.use("/tweets", tweetsRoutes);
+app.use('/tweets', tweetsRoutes);
 
-app.listen(PORT, () => {
-  console.log("Example app listening on port " + PORT);
+app.listen(process.env.PORT || 8080, () => {
+	console.log('Example app listening on port ' + PORT);
 });
